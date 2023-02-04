6d463853a6f766713e5c13e33720fb5a1cf37854
Pumas <l.a.r.p@icloud.com>
2023-02-04 21:03:10 +0300

    add fastapi and rout load data

--- src/main.py
+++ src/main.py
@@ -3,5 +3,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+@app.post('/load_data')
+async def load_data(data: dict):
+    pass
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
+
