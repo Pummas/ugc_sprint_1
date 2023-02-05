a9de4328b5123d0302e160353940956f9a1b2ad9
Pumas <l.a.r.p@icloud.com>
2023-02-05 16:02:02 +0300

    add load data in clickhouse

--- etl/etl.py
+++ etl/etl.py
@@ -1,18 +1,59 @@
+from clickhouse_driver import Client
 
 
 class MessageBrocker:
     def __init__(self, client):
         self.client = client
 
+    def extract(self):
+        pass
+
+
 class Database:
     def __init__(self, client):
         self.client = client
 
-class ETL:
-    def __init__(self, message_brocker:MessageBrocker, database: Database):
+    def load(self, data: list):
         pass
 
 
+class Clickhouse(Database):
+
+    def load(self, data):
+        try:
+
+            self.client.execute(
+                "INSERT INTO default.viewed_films (user_id, film_id, film_minutes) VALUES",
+                ((elem[0], elem[1], elem[2]) for elem in data))
+            test = self.client.execute('SELECT * FROM default.viewed_films ')
+            print(test)
+
+        except ValueError as e:
+            print(e)
+            return
+        except Exception as e:
+            print(e)
+
+
+db_client = Client(host="localhost")
+
+
+class ETL:
+    def __init__(self, message_brocker: MessageBrocker, database: Database):
+        self.message_brocker = message_brocker
+        self.database = database
+
+    def transform(self, data) -> list:
+        pass
+
+    def run(self):
+        while True:
+            data = self.message_brocker.extract()
+            tranformed_data = self.transform(data)
+            self.database.load(tranformed_data)
+
 
 if __name__ == "__main__":
-    pass
+    client = Clickhouse(db_client)
+    test = [(1, 1, 2), (2, 1, 3), (3, 1, 4), (5, 1, 6)]
+    client.load(test)
--- etl/sql_requests.py
+++ etl/sql_requests.py
@@ -1,46 +1,41 @@
-Директория для ETL процесса
-
-Для запуска Clickhouse необходимо помимо запуска контейнеров, добавить в кликхаус таблицы 
-
-```
-docker exec -it clickhouse-node1 bash
-clickhouse-client 
-CREATE DATABASE IF NOT EXISTS shard; 
-CREATE DATABASE IF NOT EXISTS replica;
-
-CREATE TABLE IF NOT EXISTS 
+create_shard = "CREATE DATABASE IF NOT EXISTS shard;"
+create_replica = "CREATE DATABASE IF NOT EXISTS replica;"
+create_shard_table = """
+CREATE TABLE 
+IF NOT EXISTS 
 shard.viewed_films (user_id Int64, film_id Int64,  film_minutes Int64) 
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/viewed_films', 'replica_1') 
 PARTITION BY film_id ORDER BY user_id;
-
+"""
+create_replica_table = """
 CREATE TABLE IF NOT EXISTS 
 replica.viewed_films (user_id Int64, film_id Int64,  film_minutes Int64) 
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/viewed_films', 'replica_2') 
 PARTITION BY film_id ORDER BY user_id;
-
+                       """
+create_default_table = """
 CREATE TABLE IF NOT EXISTS 
 default.viewed_films 
 (user_id Int64, film_id Int64,  film_minutes Int64) 
 ENGINE = Distributed('company_cluster', '', viewed_films, rand());
-
-
-docker exec -it clickhouse-node3 bash
-clickhouse-client 
-CREATE DATABASE IF NOT EXISTS shard; 
-CREATE DATABASE IF NOT EXISTS replica;
-
+                       """
+create_shard2_table = """
 CREATE TABLE IF NOT EXISTS 
 shard.viewed_films (user_id Int64, film_id Int64,  film_minutes Int64) 
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/viewed_films', 'replica_1') 
 PARTITION BY film_id ORDER BY user_id;
+"""
 
+create_replica2_table = """
 CREATE TABLE IF NOT EXISTS 
 replica.viewed_films (user_id Int64, film_id Int64,  film_minutes Int64) 
 Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/viewed_films', 'replica_2') 
 PARTITION BY film_id ORDER BY user_id;
+"""
 
+create_default2_table = """
 CREATE TABLE IF NOT EXISTS 
 default.viewed_films 
 (user_id Int64, film_id Int64,  film_minutes Int64) 
 ENGINE = Distributed('company_cluster', '', viewed_films, rand());
-```
+"""
