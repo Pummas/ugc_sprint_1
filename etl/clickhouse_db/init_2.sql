c603dcfeb715ca14b39243e8636cafd96311b110
Sergey Morichev <s.morichev@yandex.ru>
2023-02-14 22:23:16 +0500

    fix cliclhouse config

---
+++ etl/clickhouse_db/init_2.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS shard1;
+
+CREATE TABLE IF NOT EXISTS
+shard1.viewed_films (user_id UUID, film_id UUID,  pos_start Int64, pos_end Int64, created_at DateTime DEFAULT now())
+Engine=ReplicatedMergeTree('/clickhouse/tables/shard1/viewed_films', 'replica_2')
+PARTITION BY toYYYYMMDD(created_at) ORDER BY film_id;
+
+CREATE TABLE IF NOT EXISTS
+viewed_films
+(user_id UUID, film_id UUID,  pos_start Int64, pos_end Int64, created_at DateTime DEFAULT now())
+ENGINE = Distributed('company_cluster', '', viewed_films, rand());
---
+++ etl/clickhouse_db/init_3.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS shard2;
+
+CREATE TABLE IF NOT EXISTS
+shard2.viewed_films (user_id UUID, film_id UUID,  pos_start Int64, pos_end Int64, created_at DateTime DEFAULT now())
+Engine=ReplicatedMergeTree('/clickhouse/tables/shard2/viewed_films', 'replica_1')
+PARTITION BY toYYYYMMDD(created_at) ORDER BY film_id;
+
+CREATE TABLE IF NOT EXISTS
+viewed_films
+(user_id UUID, film_id UUID,  pos_start Int64, pos_end Int64, created_at DateTime DEFAULT now())
+ENGINE = Distributed('company_cluster', '', viewed_films, rand());
