6a07304ee6f804ad619280549dcc24f7e1dcc191
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2023-02-27 14:45:03 +0300

    add core code

---
+++ research/r2/src/models.py
@@ -0,0 +1,84 @@
+import datetime
+
+from core_model import CoreModel
+from uuid import UUID
+
+
+class User(CoreModel):
+    user_id: UUID
+    name: str
+
+
+class Movie(CoreModel):
+    movie_id: UUID
+    name: str
+
+
+class MovieBookmarks(CoreModel):
+    user: User
+    movies: list[Movie]
+
+
+class UserLike(CoreModel):
+    user: User
+    like: int
+
+
+class MovieLikes(CoreModel):
+    movie: Movie
+    likes: list[UserLike]
+
+
+class UserReview(CoreModel):
+    user: User
+    timestamp: datetime.datetime
+    text: str
+    likes: list[UserLike] | None
+
+
+class MovieReviews(CoreModel):
+    movie: Movie
+    reviews: list[UserReview]
+
+
+class ExUser(User):
+    bookmarks: list[Movie]
+
+
+class ExMovie(Movie):
+    reviews: list[UserReview]
+
+
+# ------------------------------------------------------------------------------ #
+
+# Классы ниже описывают предпологаемые события от пользователя
+class EventBookmark(CoreModel):
+    """Закладка на фильм"""
+
+    user_id: UUID
+    movie_id: UUID
+
+
+class EventLike(CoreModel):
+    """Лайк на фильм"""
+
+    user_id: UUID
+    movie_id: UUID
+    like: int
+
+
+class EventReview(EventBookmark):
+    """Комментарий на фильм"""
+
+    user_id: UUID
+    movie_id: UUID
+    text: str
+    created_at: datetime.datetime
+
+
+class EventReviewLike(EventLike):
+    """Лайк на обзор - или у обзора будет свой id?"""
+
+    user_id: UUID
+    movie_id: UUID
+    like: int
