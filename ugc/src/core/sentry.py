276b1b8362645a12792d333c8dc1dc06ab2585e7
Sergey Morichev <s.morichev@yandex.ru>
2023-02-28 19:07:42 +0500

    remove unnecessary handling of aiokafka logs

--- ugc/src/core/sentry.py
+++ ugc/src/core/sentry.py
@@ -4,13 +4,6 @@ from sentry_sdk.integrations.logging import LoggingIntegration
 from core.config import settings
 
 
-def before_send(event, hint):
-    # не отправляем в sentry сообщения об ошибках из логов aiokafka
-    if event.get("logger", None) == "aiokafka":
-        return None
-    return event
-
-
 def init_sentry():
     if settings.ENABLE_SENTRY:
         sentry_sdk.init(
@@ -20,6 +13,5 @@ def init_sentry():
             request_bodies="medium",
             sample_rate=1.0,
             traces_sample_rate=0.0,
-            before_send=before_send,
             integrations=[LoggingIntegration(event_level=None)],
         )
